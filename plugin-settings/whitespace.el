15cfd799138d309b88749e664599c08662fd9870
Yosuke <yosuketto@gmail.com>
2015-05-07 22:21:55 +0900

    plugin settings

---
+++ plugin-settings/whitespace.el
@@ -0,0 +1,40 @@
+;; setting whitespace.el
+(require 'whitespace)
+(setq whitespace-line-column 120)
+(setq whitespace-style '(space-mark tab-mark face spaces trailing lines-tail))
+(setq whitespace-display-mappings
+      '(
+        ;; (space-mark   ?\     [?\u00B7]     [?.]) ; space - centered dot
+        (space-mark   ?\xA0  [?\u00A4]     [?_]) ; hard space - currency
+        (space-mark   ?\x8A0 [?\x8A4]      [?_]) ; hard space - currency
+        (space-mark   ?\x920 [?\x924]      [?_]) ; hard space - currency
+        (space-mark   ?\xE20 [?\xE24]      [?_]) ; hard space - currency
+        (space-mark   ?\xF20 [?\xF24]      [?_]) ; hard space - currency
+        (space-mark ?\u3000 [?\u25a1] [?_ ?_]) ; full-width-space - square
+        ;; NEWLINE is displayed using the face `whitespace-newline'
+        (newline-mark ?\n    [?$ ?\n])  ; eol - dollar sign
+        ;; (newline-mark ?\n    [?\u21B5 ?\n] [?$ ?\n]); eol - downwards arrow
+        ;; (newline-mark ?\n    [?\u00B6 ?\n] [?$ ?\n]); eol - pilcrow
+        ;; (newline-mark ?\n    [?\x8AF ?\n]  [?$ ?\n]); eol - overscore
+        ;; (newline-mark ?\n    [?\x8AC ?\n]  [?$ ?\n]); eol - negation
+        ;; (newline-mark ?\n    [?\x8B0 ?\n]  [?$ ?\n]); eol - grade
+        ;;
+        ;; WARNING: the mapping below has a problem.
+        ;; When a TAB occupies exactly one column, it will display the
+        ;; character ?\xBB at that column followed by a TAB which goes to
+        ;; the next TAB column.
+        ;; If this is a problem for you, please, comment the line below.
+        (tab-mark     ?\t    [?\u00BB ?\t] [?\\ ?\t]) ; tab - left quote mark
+        ))
+
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+(set-face-foreground 'whitespace-space "wheat")
+(set-face-background 'whitespace-space 'nil)
+
+(set-face-underline  'whitespace-trailing t)
+(set-face-foreground 'whitespace-trailing "wheat")
+(set-face-background 'whitespace-trailing 'nil)
+
+(set-face-background 'whitespace-line "wheat")
+
+(global-whitespace-mode 1)
